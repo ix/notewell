00c376952078a6c9b625c08006fc68aedbc1bb1b
Rose <rose@lain.org.uk>
2019-09-16 02:40:35 +0100

    added a basic file opening sequence

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,23 @@
 
 module Main where
 
-import           Data.Text                      ( Text )
+import           Data.Text                      ( Text
+                                                , pack
+                                                )
 import           Control.Monad                  ( void )
-import           GI.Gtk                         ( Window (..), TextView (..))
+import           GI.Gtk                         ( Window(..)
+                                                , TextView(..)
+                                                , FileChooserButton(..)
+                                                , Button(..)
+                                                , Label(..)
+                                                , fileChooserGetFilename
+                                                )
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
 
-data State = Initial
+data State = Fileless | FileOpened FilePath
 
-data Event = Closed
+data Event = Closed | FileSelected (Maybe FilePath)
 
 view' :: State -> AppView Window Event
 view' s =
@@ -20,18 +28,24 @@ view' s =
       Window
       [ #title := "Bene"
       , on #deleteEvent (const (True, Closed))
-      , #widthRequest := 400
+      , #widthRequest := 480
       , #heightRequest := 300
       ]
     $ case s of
-        Initial -> widget TextView []
+        Fileless -> widget
+          FileChooserButton
+          [onM #selectionChanged (fmap FileSelected . fileChooserGetFilename)]
+        FileOpened file -> widget Label [#label := pack file]
 
 update' :: State -> Event -> Transition State Event
-update' _ Closed = Exit
+update' _ (FileSelected (Just file)) =
+  Transition (FileOpened file) (return Nothing)
+update' s (FileSelected Nothing) = Transition s (return Nothing)
+update' _ Closed                 = Exit
 
 main :: IO ()
 main = void $ run App { view         = view'
                       , update       = update'
                       , inputs       = []
-                      , initialState = Initial
+                      , initialState = Fileless
                       }
