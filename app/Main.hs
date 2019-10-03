808661058b3fde2b90952aa267b3a59692fb19fa
Rose <rose@lain.org.uk>
2019-10-04 00:34:50 +0100

    the worst possible way to purify textbuffer

--- app/Main.hs
+++ app/Main.hs
@@ -9,31 +9,42 @@ import qualified Data.Text                     as T
 import qualified Data.Text.IO                  as T
 import qualified Data.Text.Encoding            as T
 import           Control.Monad
+import           Control.Applicative
 import           Control.Concurrent.Async       ( async )
 import qualified GI.Gdk                        as Gdk
 import qualified GI.Gtk                        as Gtk
 import qualified Data.ByteString.Char8         as BS
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
+import           System.IO.Unsafe
 import           Bene.Renderer
 import           Paths_bene
+import           Control.Monad.Trans.State
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
 
-data State = Welcome | FileSelection | Editing (IO Text) | FileSaving (IO Text)
+data Screen = Welcome | Editing | Save | Open
 
-data Event = Closed | FileSelected (Maybe FilePath) | NewDocument | OpenDocument | Typed | Saved | SaveFileSelected (Maybe FilePath)
+data Luggage = Luggage { screen   :: Screen
+                       , buffer   :: Gtk.TextBuffer
+                       , filename :: Maybe FilePath }
 
--- | Create a TextBuffer from an optional string.
--- If one is provided, the buffer is populated
--- with the file's contents.
-createBuffer :: Maybe Text -> IO Gtk.TextBuffer
-createBuffer maybeText = do
+type AppState a = State Luggage a
+
+data Event = Closed
+           | OpenFileSelected (Maybe FilePath)
+           | NewClicked
+           | OpenClicked
+           | Typed
+           | SaveClicked
+           | SaveFileSelected (Maybe FilePath)
+
+-- | Create an empty TextBuffer.
+createBuffer :: Gtk.TextBuffer
+createBuffer = unsafePerformIO $ do
   buffer <- Gtk.textBufferNew . Just =<< markdownTextTagTable
   Gtk.on buffer #endUserAction (renderMarkdown buffer)
-  case maybeText of
-    Nothing       -> return buffer
-    Just contents -> do
-      Gtk.textBufferSetText buffer contents $ fromIntegral $ bytes contents
-      return buffer
+  return buffer
 
 -- | Constructs a FileFilter which pertains to Markdown documents.
 markdownFileFilter :: IO Gtk.FileFilter
@@ -46,7 +57,7 @@ markdownFileFilter = do
   return filt
 
 -- | Create a TextView widget from a given TextBuffer.
-editor :: IO Gtk.TextBuffer -> Widget Event
+editor :: Gtk.TextBuffer -> Widget Event
 editor buffer = widget
   Gtk.TextView
   [ afterCreated setBuffer
@@ -54,7 +65,7 @@ editor buffer = widget
   , #margin := 10
   , classes ["editor"]
   ]
-  where setBuffer tv = Gtk.textViewSetBuffer tv . Just =<< buffer
+  where setBuffer tv = Gtk.textViewSetBuffer tv $ Just buffer
 
 -- | Render Markdown based on the content of a TextBuffer.
 renderMarkdown :: Gtk.TextBuffer -> IO ()
@@ -67,79 +78,69 @@ clearTags buffer = do
   e <- Gtk.textBufferGetEndIter buffer
   Gtk.textBufferRemoveAllTags buffer s e
 
+getBufferContents :: Gtk.TextBuffer -> IO Text
+getBufferContents buffer = do
+  s <- Gtk.textBufferGetStartIter buffer
+  e <- Gtk.textBufferGetEndIter buffer
+  Gtk.textBufferGetText buffer s e True
+
 -- | Place a widget inside a BoxChild and allow it to expand.
 expandableChild :: Widget a -> BoxChild a
 expandableChild =
   BoxChild defaultBoxChildProperties { expand = True, fill = True }
 
-view' :: State -> AppView Gtk.Window Event
-view' s =
-  bin
-      Gtk.Window
-      [ #title := "Bene"
-      , on #deleteEvent (const (True, Closed))
-      , #widthRequest := 480
-      , #heightRequest := 300
-      ]
+view' :: AppState (AppView Gtk.Window Event)
+view' = do
+  s <- gets screen
+  b <- gets buffer
+  return
+    $ bin
+        Gtk.Window
+        [ #title := "Bene"
+        , on #deleteEvent (const (True, Closed))
+        , #widthRequest := 480
+        , #heightRequest := 300
+        ]
     $ case s of
         Welcome ->
           container Gtk.Box [#orientation := Gtk.OrientationHorizontal]
             $ [ expandableChild $ widget
                 Gtk.ToolButton
                 [ #iconName := "document-new"
-                , on #clicked NewDocument
+                , on #clicked NewClicked
                 , classes ["intro"]
                 ]
               , expandableChild $ widget
                 Gtk.ToolButton
                 [ #iconName := "document-open"
-                , on #clicked OpenDocument
+                , on #clicked OpenClicked
                 , classes ["intro"]
                 ]
               ]
-        FileSelection -> widget
+        Editing ->
+          container Gtk.Box [#orientation := Gtk.OrientationVertical]
+            $ [expandableChild $ bin Gtk.ScrolledWindow [] $ editor b, expandableChild $ widget Gtk.Button [on #clicked SaveClicked]]
+        Save -> widget
           Gtk.FileChooserWidget
-          [ afterCreated
-            $ \fc -> Gtk.fileChooserAddFilter fc =<< markdownFileFilter
-          , onM #fileActivated (fmap FileSelected . Gtk.fileChooserGetFilename)
-          ]
-        FileSaving buffer -> widget
-          Gtk.FileChooserWidget
-          [ #action := Gtk.FileChooserActionSave
+          [ #action := Gtk.FileChooserActionOpen
           , onM #fileActivated (fmap SaveFileSelected . Gtk.fileChooserGetFilename)
           ]
-        Editing content ->
-          container Gtk.Box [#orientation := Gtk.OrientationVertical]
-            $ [ expandableChild $ bin Gtk.ScrolledWindow [] $ editor $ createBuffer . Just =<< content
-              , container
-                Gtk.Box
-                [#orientation := Gtk.OrientationHorizontal, classes ["toolbar"]]
-                [widget Gtk.Button [#label := "Save", on #clicked Saved]]
-              ]
 
-update' :: State -> Event -> Transition State Event
-update' _ (FileSelected (Just file)) =
-  Transition (Editing $ T.readFile file) $ return Nothing
-update' s (FileSelected Nothing) = Transition s $ return Nothing
-update' _ NewDocument =
-  Transition (Editing $ return T.empty) $ return Nothing
-update' _ OpenDocument = Transition FileSelection $ return Nothing
-update' (Editing content) Saved = Transition (FileSaving content) $ return Nothing
-update' (FileSaving content) (SaveFileSelected (Just file)) = Transition (Editing content) $ do
-  T.putStrLn =<< content
-  return Nothing
-update' (FileSaving buffer) (SaveFileSelected Nothing) = Transition (Editing buffer) $ return Nothing
-update' s Typed        = Transition s $ return Nothing
-update' _ Closed       = Exit
+
+update' :: Luggage -> Event -> Transition Luggage Event
+update' s NewClicked                     = Transition s { screen = Editing } $ return Nothing
+update' s SaveClicked                    = Transition s { screen = Save } $ return Nothing
+update' s (SaveFileSelected (Just file)) = Transition s { screen = Editing } $ return Nothing
+update' _ _          = Exit
 
 main :: IO ()
 main = do
   void $ Gtk.init Nothing
 
   path     <- T.pack <$> getDataFileName "themes/giorno/giorno.css"
-
   screen   <- maybe (fail "No screen?") return =<< Gdk.screenGetDefault
   provider <- Gtk.cssProviderNew
+
   Gtk.cssProviderLoadFromPath provider path
   Gtk.styleContextAddProviderForScreen
     screen
@@ -147,9 +148,12 @@ main = do
     (fromIntegral Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
   void . async $ do
-    void $ runLoop app
+    runLoop app
     Gtk.mainQuit
   Gtk.main
  where
-  app =
-    App { view = view', update = update', inputs = [], initialState = Welcome }
+  app = App { view         = evalState view'
+            , update       = update'
+            , inputs       = []
+            , initialState = Luggage Welcome createBuffer Nothing
+            }
