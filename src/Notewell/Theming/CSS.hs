9e077cbaff694425d1ec53564e9049c0a59ff715
Rose <rose@lain.org.uk>
2019-10-31 19:24:05 +0000

    translate multiline-string css into a shakespeare text quasiquotation

--- src/Notewell/Theming/CSS.hs
+++ src/Notewell/Theming/CSS.hs
@@ -7,86 +7,82 @@
 -}
 
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Notewell.Theming.CSS where
 
 import           Notewell.Theming
 import           Prelude                 hiding ( (++) )
-import Data.Text.Encoding
-import Data.ByteString.Char8
+import           Data.Text.Encoding
+import           Data.ByteString.Char8
+import           Text.Shakespeare.Text
 
 buildCSS :: Theme -> ByteString
-buildCSS theme =
-  "\
-\@define-color bg_color "
-    ++ bgColor
-    ++ ";\
-\@define-color accent_color "
-    ++ acColor
-    ++ ";\
-\@define-color fg_color "
-    ++ fgColor
-    ++ ";\
-\.intro button {\
-\    border: none;\
-\    border-radius: 0px;\
-\    margin: 0;\
-\    padding: 0;\
-\    background: @bg_color;\
-\    font-size: 1.2em;\
-\    box-shadow: none;\
-\}\
-\.intro button:active {\
-\    background: @accent_color;\
-\}\
-\decoration {\
-\    background: @bg_color;\
-\}\
-\scrolledwindow { background: @bg_color; }\
-\.editor {\
-\    font-family: "
-    ++ bf
-    ++ ";\
-\    font-size: 1.25em;\
-\    background: @bg_color;\
-\    caret-color: @accent_color;\
-\    -GtkWidget-cursor-aspect-ratio: 0.05;\
-\    animation: blink 0.5s ease-in-out alternate infinite;\
-\}\
-\@keyframes blink {\
-\    0% { caret-color: @accent_color; }\
-\    100% { caret-color: rgba(0, 0, 0, 0); }\
-\}\
-\.editor text {\
-\    background: @bg_color;\
-\    color: @fg_color;\
-\}\
-\.editor text selection {\
-\    background: @accent_color;\
-\    color: @bg_color;\
-\}\
-\.toolbar { background: " ++ toolbar ++ "; border-top: 1px solid " ++ border ++ "; }\
-\.toolbar button { \
-\    background: " ++ toolbar ++ ";\
-\    border: none;\
-\    box-shadow: none;\
-\    text-shadow: none;\
-\    color: " ++ fgColor ++ ";\
-\    border-radius: 0px;\
-\    padding: 0em 0.5em 0em 0.5em;\
-\}\
-\.toolbar button:hover {\
-\    background: " ++ border ++ ";\
-\    color: @accent_color;\
-\    transition: color 0.5s;\
-\}\
-\.toolbar button:active { background: " ++ border ++ "; }\
-\.toolbar label { padding: 0em 1em 0em 1em; }"
+buildCSS theme = encodeUtf8 $ [st|
+  @define-color bg_color #{bgColor};
+  @define-color accent_color #{acColor};
+  @define-color fg_color #{fgColor};
+  .intro button {
+      border: none;
+      border-radius: 0px;
+      margin: 0;
+      padding: 0;
+      background: @bg_color;
+      font-size: 1.2em;
+      box-shadow: none;
+  }
+  .intro button:active {
+      background: @accent_color;
+  }
+  decoration {
+      background: @bg_color;
+  }
+  scrolledwindow { background: @bg_color; }
+  .editor {
+      font-family: #{bf};
+      font-size: 1.25em;
+      background: @bg_color;
+      caret-color: @accent_color;
+      -GtkWidget-cursor-aspect-ratio: 0.05;
+      animation: blink 0.5s ease-in-out alternate infinite;
+  }
+  @keyframes blink {
+      0% { caret-color: @accent_color; }
+      100% { caret-color: rgba(0, 0, 0, 0); }
+  }
+  .editor text {
+      background: @bg_color;
+      color: @fg_color;
+  }
+  .editor text selection {
+      background: @accent_color;
+      color: @bg_color;
+  }
+  .toolbar { 
+      background: #{toolbar};
+      border-top: 1px solid #{border};
+  }
+  .toolbar button {
+      background: #{toolbar};
+      border: none;
+      box-shadow: none;
+      text-shadow: none;
+      color: #{fgColor};
+      border-radius: 0px;
+      padding: 0em 0.5em 0em 0.5em;
+  }
+  .toolbar button:hover {
+      background: #{border};
+      color: @accent_color;
+      transition: color 0.5s;
+  }
+  .toolbar button:active { background: #{border}; }
+  .toolbar label { padding: 0em 1em 0em 1em; } 
+ |]
  where
-  (++)    = append
-  bgColor = encodeUtf8 $ background theme
-  fgColor = encodeUtf8 $ foreground theme
-  acColor = encodeUtf8 $ accent theme
-  toolbar = encodeUtf8 $ toolbarColor theme
-  border  = encodeUtf8 $ borderColor theme
-  bf      = encodeUtf8 $ bodyFont theme
+  bgColor = background theme
+  fgColor = foreground theme
+  acColor = accent theme
+  toolbar = toolbarColor theme
+  border  = borderColor theme
+  bf      = bodyFont theme
